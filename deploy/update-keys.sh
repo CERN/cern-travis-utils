2680069668c819192a81e9233574bce82a4e9b5a
bcopy <brice.copy@cern.ch>
2018-05-30 19:19:36 +0200

    change gpg export format

--- deploy/update-keys.sh
+++ deploy/update-keys.sh
@@ -1,4 +1,4 @@
-gpg --output pubring.gpg --armor --export 2F053ADC
-gpg --output secring.gpg --armor --export-secret-key 2F053ADC
+gpg --output pubring.gpg --export 2F053ADC
+gpg --output secring.gpg --export-secret-key 2F053ADC
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg -out secring.gpg.enc
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg -out pubring.gpg.enc

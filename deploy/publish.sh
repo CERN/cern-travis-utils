afc8551676e47b42fde665b8bda8b838a5a639a6
bcopy <brice.copy@cern.ch>
2018-05-30 09:32:54 +0200

    Add echo if no deploy

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -3,4 +3,6 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
+else
+    echo "Not deploying - TRAVIS PULL REQUEST = $TRAVIS_PULL_REQUEST"
 fi
